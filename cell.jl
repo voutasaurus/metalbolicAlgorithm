b3ec0f395347479499a344b2a70c92d75b53c4c2
Myles ONeill <myles.oneill@gmail.com>
2015-03-28 03:27:54 -0700

    added a basic framework for the entire program

--- cell.jl
+++ cell.jl
@@ -1,16 +1,87 @@
-println("Initiate Cell")
+# cell.jl
 
-#main loop, continue until output is reached
+# This is the main script, set up input.jl and genetics.jl then run this in Julia.
+
+###### Initial Set Up ######
+println("Creating Cell")
 
 
-#end condition flag for main loop
-complete = false
+# Set Up Input Data Structure to be Called
+# This is to be modified by the user (problem defintions)
 
-while complete == false
-	println("No!")
-	#complete = true
-	#lol
+type Molecule
+	# data stored in molecule
+	data 
+	# outside (false) or inside (true) of cell
+	location
 end
 
-#new dumb comment!
+include("input.jl")
+
+# Set Up Genetics Data Structure to be Referenced
+# This is to be modified by the user (problem solution)
+
+type Gene
+	# tree structure
+	parent
+	# access, modified by proteins
+	access
+	# name of protein it creates
+	protein
+	# length of protein life
+	lifetime
+end
+
+
+type ProteinAbundance 
+	#Protein Type = Gene Type
+	protein
+	# Percentage as a float
+	abundance
+end
+
+include("genetics.jl")
+
+
+ 
+###### Main Loop ######
+println("Cell is Alive")
+
+complete = false
+while complete == false
+
+	# NB. This is probably the wrong way to do this because of parallel processes ^.^
+	# Plz to be fixes this?
+
+
+	# (1) Inputter Process 
+	# Adds new input molecules from input data structure in a way defined here
+
+
+	# (2) Modify Protein Amounts Proteins
+	# Modifies the number of proteins in a unified way
+	# Adds based on genetics tree values
+	# Removes based on protein types
+
+	# You will be modifying the proteinDistribution array and the abundance property within the ProteinAbundance tuples inside it
+
+
+	# (3) Pair Proteins and Molecules somehow and run their scripts in parallel somehow!
+
+
+	# (4) Process checking output to see if the entire process is done and can be ended
+
+
+	#=
+		if <condition> = true
+			complete = true
+		end
+	=#
+
+	complete = true
+
+end
+
+###### Final Steps ######
+println("Cell is Dead")
 
---
+++ input.jl
@@ -0,0 +1,21 @@
+# input.jl
+
+# This script defines the problem being solved in the form of a data structure.
+
+# Modify the data structure manually to change the problem
+# In the future we'd want scripts that take data in other forms and output to new input.jl files
+
+
+###### Example Input ######
+
+println("Input Importing Begun")
+
+m1 = Molecule(1,false)
+m2 = Molecule(0,false)
+m3 = Molecule(1,false)
+m4 = Molecule(1,false)
+m5 = Molecule(0,false)
+m6 = Molecule(1,false)
+
+
+println("Input Importing Finished")
